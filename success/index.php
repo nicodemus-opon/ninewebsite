2ad0fd94af45c8de365371f9da5e0adbc61f96e2
Nicodemus Opon <nicodemusopon@gmail.com>
2020-04-06 17:44:01 +0300

    2020-04-06T17:44

--- success/index.php
+++ success/index.php
@@ -315,7 +315,7 @@ $header = "From:hello@niconinebeats \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
 
-//$retval = mail($to, $subject, $message, $header);
+$retval = mail($to, $subject, $message, $header);
 $retval="test";
 if ($retval == true) {
     echo "Message sent successfully...";
