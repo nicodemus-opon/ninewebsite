79e25011c9573cee3c54de8af87bef75418ccfe1
Nicodemus Opon <nicodemusopon@gmail.com>
2020-05-12 16:08:37 +0300

    2020-05-12T16:08

--- include/tracks.php
+++ include/tracks.php
@@ -13,7 +13,7 @@ while ($row = $result->fetch_assoc()) { ?>
         <div class="list-item r">
             <div class="media">
                 <a href="../track/?q=<?php echo $row['idx']; ?>"
-                   class="ajaxh media-content lazyload" data-bg=="../upload/<?php echo $row['image']; ?>"
+                   class="ajaxh media-content lazyload" data-bg="../upload/<?php echo $row['image']; ?>"
                    styleb="background-image:url('../upload/<?php echo $row['image']; ?>')"></a>
                 <div class="media-action media-action-overlay">
                     <button
